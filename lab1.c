a35b54496b1cb4a3c0ede92af15ee15a8bb05ef4
ser-mei <sergio.meirone@usach.cl>
2023-05-29 00:58:19 -0400

    Se agregó la función para medir el tiempo

--- lab1.c
+++ lab1.c
@@ -6,11 +6,11 @@ int main()
 {
     const char elgamalp[] = "FFFFFFFF FFFFFFFF ADF85458 A2BB4A9A AFDC5620 273D3CF1 D8B9C583 CE2D3695 A9E13641 146433FB CC939DCE 249B3EF9 7D2FE363 630C75D8 F681B202 AEC4617A D3DF1ED5 D5FD6561 2433F51F 5F066ED0 85636555 3DED1AF3 B557135E 7F57C935 984F0C70 E0E68B77 E2A689DA F3EFE872 1DF158A1 36ADE735 30ACCA4F 483A797A BC0AB182 B324FB61 D108A94B B2C8E3FB B96ADAB7 60D7F468 1D4F42A3 DE394DF4 AE56EDE7 6372BB19 0B07A7C8 EE0A6D70 9E02FCE1 CDF7E2EC C03404CD 28342F61 9172FE9C E98583FF 8E4F1232 EEF28183 C3FE3B1B 4C6FAD73 3BB5FCBC 2EC22005 C58EF183 7D1683B2 C6F34A26 C1B2EFFA 886B4238 61285C97 FFFFFFFF FFFFFFFF";
 
-//Declare random number and state
+//Declare variables and state
     mpz_t p, g, secretKey, publicKey, message, ephemeralKey, cipher1, cipher2, c1inverse, decrypted;
     gmp_randstate_t state;
 
-//Initialize random number and state
+//Initialize variables and state
     mpz_init(p);
     mpz_init(g);
     mpz_init(secretKey);
@@ -62,6 +62,11 @@ int main()
         gmp_printf("GCD: %Zd\n", factor);
     }*/
 
+//Declare time variables
+    clock_t time1, time2;
+
+    time1 = clock();
+
 //Set p and g
     mpz_set_str(p, elgamalp, 16);
     mpz_set_ui(g, 2);
@@ -77,7 +82,9 @@ int main()
     gmp_printf("Public Key: %#ZX\n", publicKey);
 
 //Generate random Bob message m
-    mpz_urandomm(message, state, p);
+
+    //mpz_urandomm(message, state, p);
+    mpz_set_str(message, "FFFFFF", 16);
     gmp_printf("Message: %#ZX\n", message);
 
 //Generate random ephemeral key k
@@ -103,6 +110,9 @@ int main()
     mpz_mul(decrypted, c1inverse, cipher2);
     mpz_mod(decrypted, decrypted, p);
     gmp_printf("Decrypted Message: %#ZX\n", decrypted);
+
+    time2 = clock();
+    printf("Time: %f\n" , ( (double)time2 - (double)time1 ) / ( (double)CLOCKS_PER_SEC ) );    
 
 //Compare decrypted message to original message
     if(mpz_cmp(message, decrypted) == 0)
